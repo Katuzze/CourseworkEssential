c8b54d81186539a0212c91a529a97ab303dfc371
Katuzze <128620758+Katuzze@users.noreply.github.com>
2023-09-10 21:06:43 +0300

    Course work

---
+++ courseEssential/courseEssential/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace courseEssential
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("квадратне рівняння");
+            Console.WriteLine("ax² + bx + c = 0");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("━━━━━━━━━━━━━━━━━━━━━");
+            Console.ResetColor();
+            Console.Write("Введіть значення a: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введіть значення b: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введіть значення c: ");
+            double num3 = Convert.ToDouble(Console.ReadLine());
+
+            Task task = new Task(num1, num2, num3);
+            Solver solver = new Solver(task);
+            Solution solution = solver.Solve();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("━━━━━━━━━━━━━━━━━━━━━");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine(solution);
+            Console.ResetColor();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ courseEssential/courseEssential/Solution.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace courseEssential
+{
+    class Solution
+    {
+        public double x1;
+        public double x2;
+        public Result Result;
+
+        public Solution(double x1, double x2, Result result)
+        {
+            this.x1 = x1;
+            this.x2 = x2;
+            this.Result = result;
+        }
+
+        public Solution(double x1, Result result)
+        {
+            this.x1 = x1;
+            this.Result = result;
+        }
+
+        public Solution(Result result)
+        {
+            this.Result = result;
+        }
+
+        public override string ToString()
+        {
+            switch(Result)
+            {
+                case Result.twoResult:
+                    return $"x₁ = {x1} \nx₂ = {x2}";
+                case Result.oneResult:
+                    return $"x = {x1}";
+                default:
+                    return "Немає розв'язків";
+            }
+        }
+    }
+}
